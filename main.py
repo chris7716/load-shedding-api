a459e73e3c90a305c080d6cf6aec5d5f1e059581
Hasitha Kaushan <hkraclh@gmail.com>
2021-12-09 23:20:07 +0530

    Add basic db opject mappers

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
 from fastapi import FastAPI
+from datetime import datetime
 from cassandra.cluster import Cluster
+from cassandra.cqlengine.management import sync_table
+from cassandra.cqlengine.connection import setup
+
+from storage import area_consumption
+from storage import area
+from storage import ceb_admin
+from storage import customer_area
+from storage import customer_daily_summary
+from storage import customer_data
+from storage import customer_priority
+from storage import device_daily_summary
+from storage import device_data
+from storage import device
+from storage import load_shedding
+from storage import super_admin
+from storage import user
 
 app = FastAPI()
-cluster = Cluster(['194.163.141.181'])
-session = cluster.connect()
+
+setup(['194.163.141.181'], "test", retry_connect=True)
+
+sync_table(area_consumption.AreaConsumptionModel)
+sync_table(area.AreaModel)
+sync_table(ceb_admin.CebAdminDataModel)
+sync_table(customer_area.CustomerAreaModel)
+sync_table(customer_daily_summary.CustomerDailySummaryModel)
+sync_table(customer_data.CustomerDataModel)
+sync_table(customer_priority.CustomerPriorityModel)
+sync_table(device_daily_summary.DeviceDailySummaryModel)
+sync_table(device_data.DeviceDataModel)
+sync_table(device.DeviceModel)
+sync_table(load_shedding.LoadSheddingModel)
+sync_table(super_admin.SuperAdminModel)
+sync_table(user.UserModel)
 
 @app.get("/")
 async def root():
+    #em1 = example.ExampleModel.create(example_type=0, description="example1", created_at=datetime.now())
     return {"message": "Hello World"}
